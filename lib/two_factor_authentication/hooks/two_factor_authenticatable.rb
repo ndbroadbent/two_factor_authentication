59df8d480d0410496a54ffb0b8a54429faca02ec
Nathan Broadbent <nathan.f77@gmail.com>
2013-01-17 10:22:25 +1300

    Rewrite Warden::Manager.after_authentication hook for trusted devices

--- lib/two_factor_authentication/hooks/two_factor_authenticatable.rb
+++ lib/two_factor_authentication/hooks/two_factor_authenticatable.rb
@@ -3,11 +3,9 @@ Warden::Manager.after_authentication do |user, auth, options|
 
   if user.try(:need_two_factor_authentication?, auth.request)
     uid = auth.cookies.signed[:trusted_device_uid]
-    if uid.present?
-      next if TrustedUserDevice.where(:user_id => user.id, :uid => uid).exists?
-    end
+    trusted_device = uid.present? && TrustedUserDevice.where(:user_id => user.id, :uid => uid).exists?
 
-    auth.session(options[:scope])[:need_two_factor_authentication] = true
-    user.two_factor_authentication!
+    auth.session(options[:scope])[:need_two_factor_authentication] = !trusted_device
+    user.two_factor_authentication! unless trusted_device
   end
 end
