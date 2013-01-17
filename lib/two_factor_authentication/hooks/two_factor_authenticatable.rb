24935a6aa95356ccb5cdeb9d94b9eefcd91a8e02
Nathan Broadbent <nathan.f77@gmail.com>
2013-01-18 10:23:47 +1300

    Refactored two_factor_authenticatable hook

--- lib/two_factor_authentication/hooks/two_factor_authenticatable.rb
+++ lib/two_factor_authentication/hooks/two_factor_authenticatable.rb
@@ -1,11 +1,11 @@
 Warden::Manager.after_authentication do |user, auth, options|
-  session = auth.session(options[:scope])
-
   if user.try(:need_two_factor_authentication?, auth.request)
-    uid = auth.cookies.signed[:trusted_device_uid]
-    trusted_device = uid.present? && TrustedUserDevice.where(:user_id => user.id, :uid => uid).exists?
+    if uid = auth.cookies.signed[:trusted_device_uid]
+      # Skip two-factor auth if logging in from trusted device
+      next if TrustedUserDevice.where(:user_id => user.id, :uid => uid).exists?
+    end
 
-    auth.session(options[:scope])[:need_two_factor_authentication] = !trusted_device
-    user.two_factor_authentication! unless trusted_device
+    auth.session(options[:scope])[:need_two_factor_authentication] = true
+    user.two_factor_authentication!
   end
 end
